5371c06cafc8e7f157ea918beec2a29423ac8cf5
Séverine <severine.coute@laposte.net>
2021-05-24 11:52:40 +0200

    advanced routing

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -2,7 +2,6 @@
 namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-
 use Symfony\Component\Routing\Annotation\Route;
 class ProgramController extends AbstractController
 {
@@ -22,6 +21,16 @@ class ProgramController extends AbstractController
      
          ]);
     }
+
+     /**
+        * @Route("/programs/{id}", methods={"GET"}, requirements={"id"="\d+"}, name="program_show")
+     */
+
+     public function show(int $id): Response
+
+     {
+        return $this->render('program/show.html.twig', ['id' => $id]);
+     }
 
 }
 
