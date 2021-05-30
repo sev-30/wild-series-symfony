7fe92734de50ff1a407f333eca6e17694ed2cc2c
Séverine <severine.coute@laposte.net>
2021-05-30 21:43:16 +0200

    data doctrine

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -3,7 +3,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use App\Entity\Program;
 /**
 
  * @Route("/programs", name="program_")
@@ -37,15 +37,40 @@ class ProgramController extends AbstractController
            return $this->render('program/index.html.twig', ['programs' => $programs]);
       }
 
-     /**
-        * @Route("/programs/{id}", methods={"GET"}, requirements={"id"="\d+"}, name="program_show")
-     */
+      /**
 
-     public function show(int $id): Response
+         * Getting a program by id
 
-     {
-        return $this->render('program/show.html.twig', ['id' => $id]);
-     }
+         *
 
+         * @Route("/show/{id<^[0-9]+$>}", name="show")
+
+         * @return Response
+
+         */
+
+      public function show(int $id):Response
+      {
+
+         $program = $this->getDoctrine()
+
+        ->getRepository(Program::class)
+
+        ->findOneBy(['id' => $id]);
+
+
+         if (!$program) {
+
+        throw $this->createNotFoundException(
+
+            'No program with id : '.$id.' found in program\'s table.'
+         );
+         }
+
+         return $this->render('program/show.html.twig', [
+
+        'program' => $program,
+
+          ]);
+      }
 }
-
